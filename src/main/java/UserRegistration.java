0fed402f35094dec6d38745a9ff3b6f882bdee27
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2022-12-29 13:37:12 +0530

    Added: Last Name Validation

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -3,9 +3,12 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
  public static final String FIRST_NAME_PATTERN = "^[A-Z][a-z]{2,}";
-
+public static final String  LAST_NAME_PATTERN = "^[A-Z][a-z]{2,}";
  public boolean validateFirstName(String firstName) {
   return patternChecker(firstName, FIRST_NAME_PATTERN);
+ }
+ public boolean validateLastName(String firstName) {
+  return patternChecker(firstName, LAST_NAME_PATTERN);
  }
   public boolean patternChecker (String input, String fieldPattern){
    Pattern pattern = Pattern.compile(fieldPattern);
