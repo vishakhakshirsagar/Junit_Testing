79a715db2bd05744dcfa16a7e11a0baadf3c61b8
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2023-01-03 14:11:12 +0530

    Added:Parameterised test for all valid Email Id

--- src/test/java/TestUserRegistrationTest.java
+++ src/test/java/TestUserRegistrationTest.java
@@ -1,49 +1,38 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class TestUserRegistrationTest {
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ValidateEmailIdTest {
+
+    private final boolean expectedResult;
+    private final String emailId;
+
+    public ValidateEmailIdTest(String emailId, boolean expectedResult) {
+        this.expectedResult = expectedResult;
+        this.emailId = emailId;
+    }
+
     UserRegistration userRegistration = new UserRegistration();
 
-    @Test
-    public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue() {
-        boolean result = userRegistration.validateLastName("Kshirsagar");
-        Assert.assertTrue(result);
+    @Parameterized.Parameters
+    public static Collection emailIds_With_ExpectedResult() {
+        return Arrays.asList(new Object[][]{{"abc.syz@bl.co.in", true}, {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true}, {"abc111@abc.com", true}, {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true}, {"abc@1.com", true}, {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true}, {"abc", false}, {"abc@.com.my", false}, {"abc123@gmail.a", false},
+                {"abc123@.com", false}, {"abc123@.com.com", false}, {".abc@abc.com", false},
+                {"abc()*@gmail.com", false}, {"abc@%*.com", false}, {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false}, {"abc@abc@gmail.com", false}, {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}});
     }
 
     @Test
-    public void givenLastName_WhenLessThanThreeChars_ShouldReturnFalse() {
-        boolean result = userRegistration.validateLastName("Ks");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenEmail_WhenValid_ShouldReturnTrue() {
-        boolean result = userRegistration.validateEmailAddress("abc-100@yahoo.com");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenMobileNumber_IsValid_ShouldReturnTrue() {
-        boolean result = userRegistration.validateMobileNumber("+91 9146965226");
-//        Assert.assertFalse(result);
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenPassword_whenValid_showReturnTrue() {
-        boolean result = userRegistration.validatePassword("Juik3241");
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenPassword_atLeast_oneUpperCase() {
-        boolean result = userRegistration.validatePasswordAtLeastOneUpperCase("Jui32134");
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenPassword_atLeast_oneNumericNumber() {
-        boolean result = userRegistration.validatePasswordAtLeastOneNumericNumber("Jui21@05");
-    }
-    @Test
-    public void givenPassword_atLeast_one_special_character() {
-        boolean result = userRegistration.validatePasswordAtLeastOneSpecialCharacter("Jui213@05");
+    public void givenEmailId_WhenProper_ShouldReturnExpectedResult() {
+        Assert.assertEquals(this.expectedResult, userRegistration.validateEmailAddress(emailId));
     }
 }
